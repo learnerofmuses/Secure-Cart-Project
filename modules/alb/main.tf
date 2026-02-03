3035d92a24314d5001fdf18378265baec274f318
Tyler <tyscott039@gmail.com>
2026-02-03 08:01:08 -0500

    our security group resource for our alb

---
+++ modules/alb/main.tf
@@ -0,0 +1,18 @@
+resource security_group "alb_sg" {
+    name = "alb_security_group"
+    description = "security group for our application load balancer"
+    vpc_id = var.vpc_id
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
