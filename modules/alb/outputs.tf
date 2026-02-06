c7011f508e8335f4ccc85229f2b9d47a11d4b32e
Tyler <tyscott039@gmail.com>
2026-02-05 20:44:39 -0500

    outputs for our application load balancer

---
+++ modules/alb/outputs.tf
@@ -0,0 +1,12 @@
+output "alb_security_group_id" {
+    value = aws_security_group.alb_sg.id
+}
+
+output "target_group_arn" {
+    value = aws_lb_target_group.app_lb_target_group_TS.arn
+}
+
+output "alb_dns_name" { 
+    description = "our public dns name for the application load balancer"
+    value = aws_lb.application_lb_TS.dns_name
+}
