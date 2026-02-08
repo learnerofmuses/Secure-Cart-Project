566873f15e9ab43c2b34e37b1ad6a48d0bf7f812
Tyler <tyscott039@gmail.com>
2026-02-08 16:54:22 -0500

    renamed our public subnet output and added an addition public subnet output. Added an output for our internet gateway.

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -3,17 +3,24 @@ output "vpc_id"{
     value       = aws_vpc.vpc_TS.id
 }
 
-output "public_subnet_id" { 
-    value = aws_subnet.public_subnet.id
+output "public_subnet_a_id" { 
+    value = aws_subnet.public_sub_A.id
+}
+
+output "public_subnet_b_id" {
+    value = aws_subnet.public_sub_B.id
 }
 
 output "private_subnet_a_id" {
-  value = aws_subnet.private_sub_a.id
+  value = aws_subnet.private_sub_A.id
 }
 
 output "private_subnet_b_id" {
-    value = aws_subnet.private_sub_b.id
+    value = aws_subnet.private_sub_B.id
 }
 
+output "internet_gateway_id" {
+    description = "the id of our internet gateway which attaches to our vpc and allows communication between the internet and our vpc"
+    value = aws_internet_gateway.igw_TS.id
+}
 
-#we created a separate IGW module for better modularity along with the routing tables 
